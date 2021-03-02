23fb3b905cedbbaa75d4cf1f98e5508f518e78dd
annagapuz <3227642+annagapuz@users.noreply.github.com>
2021-03-02 16:36:18 -0500

    Latest updates for demo prep

--- docker/devbox/startup.sh
+++ docker/devbox/startup.sh
@@ -2,13 +2,13 @@
 
 alternatives --set java java-11-openjdk.x86_64
 
-cd /opt/movie || exit 1
+cd /opt/movie/local
 
-svn co --non-interactive --no-auth-cache --username admin --password password http://subversion/svn/movie-service/trunk MovieService
+svn import --non-interactive --no-auth-cache --username admin --password password -m "Initial checkin" http://subversion/svn/platform/trunk/applications/movie-application
 
-svn co --non-interactive --no-auth-cache --username admin --password password http://subversion/svn/movie-web/trunk MovieWeb
+svn co --force --non-interactive --no-auth-cache --username admin --password password http://subversion/svn/platform/trunk/applications/movie-application /opt/movie/local
 
-svn co --non-interactive --no-auth-cache --username admin --password password http://subversion/svn/pipeline/trunk Pipeline
+svn propset svn:ignore .git .
 
 mvn deploy:deploy-file -DrepositoryId=demoRepo -Durl=http://nexus:8081/repository/demo-release/ -Dfile=/tmp/jconn4.jar -DgroupId=com.sybase.jdbc4 -DartifactId=jconn -Dversion=4 -Dpackaging=jar
 
